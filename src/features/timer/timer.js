7cd6856c859a9c8e740d6c7477b87687a7ef663f
tayouthae <tayouthmalla@gmail.com>
2022-07-18 21:23:25 +0545

    timer component created

--- src/features/timer/timer.js
+++ src/features/timer/timer.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { colors } from '../../utils/colors';
+import { spacing } from '../../utils/sizes';
+import { CountDown } from '../../components/CountDown';
 
-export const Timer = ({ subject }) => {
+export const Timer = ({ subject, textStyles = {} }) => {
   return (
     <View style={styles.container}>
-      <Text>Timer goes here: {subject}</Text>
+      <CountDown style={styles.centerAlign} />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>Focusing on:</Text>
+        <Text style={[styles.task, textStyles]}>{subject}</Text>
+      </View>
     </View>
   );
 };
@@ -12,5 +19,22 @@ export const Timer = ({ subject }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    paddingTop: spacing.xxl,
+  },
+  title: {
+    textAlign: 'center',
+    color: colors.white,
+  },
+  task: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: colors.white,
+  },
+  centerAlign: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 0.5,
   },
 });
